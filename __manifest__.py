56b9f5f0d6e595285aeaaa46cd948bc467b394d6
Matthew Watkins <mwatkins.bulk@gmail.com>
2017-12-06 13:56:53 -0700

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+	'name': 'Orange Theatre CRM',
+	'description': """Changes the CRM structure to better handle contacts from leads.
+					  """,
+	'author': 'Matthew Watkins',
+	'depends': ['contacts', 'sale', 'crm'],
+	'data': [	
+		'views/otg_crm_views.xml',
+	],
+}
---
+++ models/otg_crm.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api, exceptions
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class Lead(models.Model):
+	_inherit = 'crm.lead'
+
+	contact_id = fields.Many2one('res.partner', 'Contact')
+
+	@api.onchange('partner_id')
+	def _onchange_partner_id(self):
+		if self.partner_id.id != self.contact_id.parent_id.id:
+			self.contact_id = False
+
+	@api.constrains('partner_id', 'contact_id')
+	def _check_contact(self):
+		if self.partner_id.id != self.contact_id.parent_id.id:
+			raise exceptions.ValidationError"You must choose a contact that's associated with the customer account")
+
